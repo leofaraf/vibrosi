ab8713bf509aa1df7599195f49292f9272c2e048
Leon Farafonov <leofaraf@gmail.com>
2025-11-24 08:07:47 +0300

    Initial commit

---
+++ archive/convert2csv.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+projects_df = pd.read_excel("Проектные_данные_bnMAPpro.xlsx")
+projects_df.to_csv("Проектные_данные_bnMAPpro.csv")
+
+deals_df = pd.read_excel("Сделки_bnMAPpro.xlsx")
+deals_df.to_csv("Сделки_bnMAPpro.csv")
