e62be303b53715ac65bd40d957d86adaa10d568f
Leon Farafonov <leofaraf@gmail.com>
2025-11-26 19:16:15 +0300

    update: по датасету со сделками

--- services/convert2csv.py
+++ services/convert2csv.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-projects_df = pd.read_excel("Проектные_данные_bnMAPpro.xlsx")
-projects_df.to_csv("Проектные_данные_bnMAPpro.csv", index=False)
+# projects_df = pd.read_excel("Проектные_данные_bnMAPpro.xlsx")
+# projects_df.to_csv("Проектные_данные_bnMAPpro.csv", index=False)
 
-deals_df = pd.read_excel("Сделки_bnMAPpro.xlsx")
-deals_df.to_csv("Сделки_bnMAPpro.csv", index=False)
+# deals_df = pd.read_excel("Сделки_bnMAPpro.xlsx")
+# deals_df.to_csv("Сделки_bnMAPpro.csv", index=False)
+
+deals_df = pd.read_excel("offers_bnMAPpro.xlsx")
+deals_df.to_csv("offers_bnMAPpro.csv", index=False)
--- services/makeautoeda.py
+++ services/makeautoeda.py
@@ -2,27 +2,27 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 # Example: load data
-df = pd.read_csv("Сделки_bnMAPpro.csv")
+df = pd.read_csv("offers_bnMAPpro.csv")
 
 # Create the profile (the auto EDA report)
 profile = ProfileReport(
     df,
-    title="Сделки_bnMAPpro AutoEDA",
+    title="offers_bnMAPpro AutoEDA",
     explorative=True  # adds more advanced analyses & plots
 )
 
-profile.to_file("Сделки_bnMAPpro_auto_eda_report.html")
+profile.to_file("offers_bnMAPpro.html")
 print("Report saved to auto_eda_report.html")
 
-# Example: load data
-df = pd.read_csv("Проектные_данные_bnMAPpro.csv")
+# # Example: load data
+# df = pd.read_csv("Проектные_данные_bnMAPpro.csv")
 
-# Create the profile (the auto EDA report)
-profile = ProfileReport(
-    df,
-    title="Проектные_данные_bnMAPpro AutoEDA",
-    explorative=True  # adds more advanced analyses & plots
-)
+# # Create the profile (the auto EDA report)
+# profile = ProfileReport(
+#     df,
+#     title="Проектные_данные_bnMAPpro AutoEDA",
+#     explorative=True  # adds more advanced analyses & plots
+# )
 
-profile.to_file("Проектные_данные_bnMAPpro_auto_eda_report.html")
-print("Report saved to auto_eda_report.html")
+# profile.to_file("Проектные_данные_bnMAPpro_auto_eda_report.html")
+# print("Report saved to auto_eda_report.html")
