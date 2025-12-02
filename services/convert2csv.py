be46fe4929d229145ea186b834783ab63ee25dee
SokIlya <1003sia08@gmail.com>
2025-12-03 00:02:55 +0300

    Обновил данные

--- services/convert2csv.py
+++ services/convert2csv.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
-# projects_df = pd.read_excel("Проектные_данные_bnMAPpro.xlsx")
-# projects_df.to_csv("Проектные_данные_bnMAPpro.csv", index=False)
+projects_df = pd.read_excel("services/Проектные данные_2025-11-25.xlsx")
+projects_df.to_csv("projects.csv", index=False)
 
-# deals_df = pd.read_excel("Сделки_bnMAPpro.xlsx")
-# deals_df.to_csv("Сделки_bnMAPpro.csv", index=False)
-
-deals_df = pd.read_excel("offers_bnMAPpro.xlsx")
-deals_df.to_csv("offers_bnMAPpro.csv", index=False)
+deals_df = pd.read_excel("services/Сделки_2025-11-25.xlsx")
+deals_df.to_csv("offers.csv", index=False)
