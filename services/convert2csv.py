929e9db461a7d2217920ec30ffb0f63321d98763
SokIlya <1003sia08@gmail.com>
2025-12-11 11:25:26 +0300

    добавил модель

--- services/convert2csv.py
+++ services/convert2csv.py
@@ -4,4 +4,4 @@ projects_df = pd.read_excel("services/Проектные данные_2025-11-25
 projects_df.to_csv("projects.csv", index=False)
 
 deals_df = pd.read_excel("services/Сделки_2025-11-25.xlsx")
-deals_df.to_csv("offers.csv", index=False)
+deals_df.to_csv("deals.csv", index=False)
